0b9219f090c56e9f8b4e46a53253728c2e00fab8
adelpro <47066151+adelpro@users.noreply.github.com>
2025-02-11 17:27:03 +0100

    test

--- seed-server/constants/TRACKERS.js
+++ seed-server/constants/TRACKERS.js
@@ -1,7 +1,5 @@
 const TRACKERS = [
-    'ws://tracker:8083',
-    'wss://tracker.openquran.us.kg',
-    'https://tracker.openquran.us.kg/announce'
+    'ws://tracker:8083'
 ];
 
 export default TRACKERS
