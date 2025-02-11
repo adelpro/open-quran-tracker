335dfbe64078896fa92e37eeeecd9e4207425cde
adelpro <47066151+adelpro@users.noreply.github.com>
2025-02-11 19:12:34 +0100

    fix(seed): trackers list fix

--- seed-server/seed.mjs
+++ seed-server/seed.mjs
@@ -45,7 +45,25 @@ const options = {
     'https://tracker.openquran.us.kg/announce',
     'wss://tracker.openwebtorrent.com',
     'wss://tracker.btorrent.xyz',
-    'wss://tracker.webtorrent.dev']
+    'wss://tracker.webtorrent.dev',
+    "udp://tracker.coppersurfer.tk:6969",
+    "udp://tracker.leechers-paradise.org:6969",
+    "udp://tracker.opentrackr.org:1337",
+    "udp://tracker.internetwarriors.net:1337",
+    "udp://tracker.openbittorrent.com:80",
+    "udp://tracker.publicbt.com:80",
+    "udp://tracker.mg64.net:2710",
+    "udp://tracker.zer0day.to:1337",
+    "udp://explodie.org:6969",
+    "udp://tracker.empire-js.us:1337",
+    "udp://tracker.uw0.xyz:6969",
+    "udp://tracker.dler.org:6969",
+    "udp://tracker.moeking.me:6969",
+    "udp://tracker.tiny-vps.com:6969",
+    "udp://open.stealth.si:80",
+    "udp://tracker.torrent.eu.org:451",
+    "udp://tracker.cyberia.is:6969",  
+  ]
 };
 
 async function processMagnetLinks() {
