3f0b1ca3973bb52bef846c8f1ec6045a8c66b213
adelpro <47066151+adelpro@users.noreply.github.com>
2025-02-11 17:52:08 +0100

    test(seed): clietn add options

--- seed-server/constants/TRACKERS.js
+++ seed-server/constants/TRACKERS.js
@@ -1,8 +1,27 @@
 const TRACKERS = [
-    'ws://tracker:8083',
-    'wss://tracker.openwebtorrent.com',
-    'wss://tracker.btorrent.xyz',
-    'wss://tracker.webtorrent.dev'
+  "ws://tracker:8083",
+  "wss://tracker.openquran.us.kg",
+  "https://tracker.openquran.us.kg/announce",
+  "wss://tracker.openwebtorrent.com",
+  "wss://tracker.btorrent.xyz",
+  "wss://tracker.webtorrent.dev",
+  "udp://tracker.coppersurfer.tk:6969",
+  "udp://tracker.leechers-paradise.org:6969",
+  "udp://tracker.opentrackr.org:1337",
+  "udp://tracker.internetwarriors.net:1337",
+  "udp://tracker.openbittorrent.com:80",
+  "udp://tracker.publicbt.com:80",
+  "udp://tracker.mg64.net:2710",
+  "udp://tracker.zer0day.to:1337",
+  "udp://explodie.org:6969",
+  "udp://tracker.empire-js.us:1337",
+  "udp://tracker.uw0.xyz:6969",
+  "udp://tracker.dler.org:6969",
+  "udp://tracker.moeking.me:6969",
+  "udp://tracker.tiny-vps.com:6969",
+  "udp://open.stealth.si:80",
+  "udp://tracker.torrent.eu.org:451",
+  "udp://tracker.cyberia.is:6969",
 ];
 
-export default TRACKERS
+export default TRACKERS;
