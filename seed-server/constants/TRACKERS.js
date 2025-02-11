3e111f8b8962fac7e8a513c3ac7a9949b0c88fc3
adelpro <47066151+adelpro@users.noreply.github.com>
2025-02-11 14:00:42 +0100

    refactor(seed-server): add https private tracker

--- seed-server/constants/TRACKERS.js
+++ seed-server/constants/TRACKERS.js
@@ -1,7 +1,8 @@
 export const TRACKERS = [
     'ws://tracker:8083',
     'wss://tracker.openquran.us.kg',
-    'https://tracker.openquran.us.kg',
+    'https://tracker.openquran.us.kg/annance',
+    
     'udp://tracker.openwebtorrent.com',
     'wss://tracker.btorrent.xyz',
     'wss://tracker.webtorrent.dev',
