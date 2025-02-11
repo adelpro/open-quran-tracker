8366fd3b24c2171e6d305ffd052e18fa6f223710
adelpro <47066151+adelpro@users.noreply.github.com>
2025-02-11 19:50:31 +0100

    test(seed): import trackers list

--- seed-server/seed.mjs
+++ seed-server/seed.mjs
@@ -40,30 +40,7 @@ const client = new WebTorrent({ maxConns: 200, dht: true, ut_pex: true });
 
 const options = {
   path: downloadPath,
-  announce: [
-    'ws://tracker:8083',
-    'https://tracker.openquran.us.kg/announce',
-    'wss://tracker.openwebtorrent.com',
-    'wss://tracker.btorrent.xyz',
-    'wss://tracker.webtorrent.dev',
-    "udp://tracker.coppersurfer.tk:6969",
-    "udp://tracker.leechers-paradise.org:6969",
-    "udp://tracker.opentrackr.org:1337",
-    "udp://tracker.internetwarriors.net:1337",
-    "udp://tracker.openbittorrent.com:80",
-    "udp://tracker.publicbt.com:80",
-    "udp://tracker.mg64.net:2710",
-    "udp://tracker.zer0day.to:1337",
-    "udp://explodie.org:6969",
-    "udp://tracker.empire-js.us:1337",
-    "udp://tracker.uw0.xyz:6969",
-    "udp://tracker.dler.org:6969",
-    "udp://tracker.moeking.me:6969",
-    "udp://tracker.tiny-vps.com:6969",
-    "udp://open.stealth.si:80",
-    "udp://tracker.torrent.eu.org:451",
-    "udp://tracker.cyberia.is:6969",  
-  ]
+  announce: TRACKERS
 };
 
 async function processMagnetLinks() {
